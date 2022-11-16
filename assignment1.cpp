5cdf6a288751dce0a05b5530e4b4987248dda6e6
MOHAMEDAHMED12122 <118351573+MOHAMEDAHMED12122@users.noreply.github.com>
2022-11-16 12:25:18 +0200

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+struct node 
+{
+    int data;
+    node* next;
+};
+
+node* top = NULL;
+
+void display();
+void push(int value);
+int pop();
+int peek();
+
+int main() 
+{
+    push(3);
+    push(2);
+    push(1);
+    display();
+    pop();
+    display();
+    cout << "top = " << peek();
+    return 0;
+}
+
+void push (int value)
+{
+    node* new_node = new node; 
+    new_node->data = value;
+    new_node->next = top;
+    top = new_node;
+}
+
+int pop () 
+{
+    int value; 
+    if (top == NULL)
+    {
+       cout << "stack underflow\n";
+       return -1; 
+    }
+    else 
+    {
+        node* first_node = top;
+        top = first_node->next;
+        value = first_node->data;
+        delete(first_node);
+        return value;
+    }
+}
+
+int peek()
+{
+    if (top == NULL)
+    {
+       cout << "stack underflow\n";
+       return -1; 
+    }
+    else 
+    {
+        return top->data;
+    }
+}
+
+
+void display()
+{
+    node* current;
+    if (top == NULL)
+    {
+        cout << "stack underflow";
+    }
+    else 
+    {
+        current = top;
+        while (current != NULL)
+        {
+            cout << current->data << "\n";
+            current = current->next;
+        }
+    }
+    cout << endl;
+}
